b0b2bdf70b61482fac8986f3252300474f7f631b
watercsu <dingding.bupt@gmail.com>
2014-11-03 17:43:47 +0800

    Update ReverseInteger.cpp

--- ReverseInteger.cpp
+++ ReverseInteger.cpp
@@ -7,20 +7,14 @@ Example2: x = -123, return -321
 class Solution {
 public:
      int reverse(int x) {
-         int num = x, ret = 0, i = 0;
-    	 vector<int> intVec;
+         int num = x, ret = 0;
     
-    	 if (x < 0) num = x*(-1);
-    
-    	 while (num > 0)
+    	 while (num)
     	 {
-    		 intVec.push_back(num%10);
+    	     ret = ret*10+num%10;
     		 num = num / 10;
     	 }
     
-    	 while (i<intVec.size())
-    		 ret = ret * 10 + intVec[i++];
-    	 ret = x > 0 ? ret : ret*(-1);
     	 return ret;
      }
 };
