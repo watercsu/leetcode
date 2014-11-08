d445757b205498a6a5c3f7ae0e78622aa20f4765
watercsu <dingding.bupt@gmail.com>
2014-11-08 14:57:38 +0800

    Create TwoSum.cpp

---
+++ TwoSum.cpp
@@ -0,0 +1,28 @@
+/*Given an array of integers, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2  */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        	 map<int, int> hashmap;
+        	 vector<int> ret;
+        
+        	 for (int i = 0; i<numbers.size(); i++) {
+        		 int num = target - numbers[i];
+        		 if (hashmap.find(num) != hashmap.end()) {
+        			 ret.push_back(hashmap[num] + 1);
+        			 ret.push_back(i + 1);
+        		 }
+        		 else {
+        			 hashmap[numbers[i]]=i;
+        		 }
+        	 }
+        	 return ret;
+    }
+};
