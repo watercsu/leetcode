40924a72d05dbe7aa9f01ece284c41de009eb0f7
watercsu <dingding.bupt@gmail.com>
2014-12-06 21:28:57 +0800

    Create GasStation.cpp

---
+++ GasStation.cpp
@@ -0,0 +1,24 @@
+/* There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
+
+Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+
+Note:
+The solution is guaranteed to be unique. */
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int ret = 0,tmp = 0,sum = 0;
+        for(int i=0; i<gas.size(); i++) {
+            int diff = gas[i]-cost[i];
+            sum += diff;
+            tmp += diff;
+            if(tmp<0) {
+                ret=i+1;
+                tmp=0;
+            }
+        }
+        return sum<0?-1:ret;
+    }
+};
